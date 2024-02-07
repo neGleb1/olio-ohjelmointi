1d39a5e6a09f49ea330aec6e2a88585f28d06c0b
danil <daniachmetshin@gmail.com>
2024-02-07 19:33:03 +0200

    tehtävä 4 valmis

---
+++ teht4/kerrostalo.h
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+#ifndef KERROSTALO_H
+
+#define KERROSTALO_H
+
+class Asunto {
+
+public:
+
+   int asukasMaara;
+   int neliot;
+
+   Asunto();
+
+   ~Asunto();
+   
+   void maarita(int, int); 
+
+   double laskeKulutus(double);
+};
+
+class Kerros : public Asunto {
+
+public:
+   
+   Kerros();
+
+   virtual ~Kerros();
+   
+   virtual void maaritaAsunnot(); 
+
+   virtual double laskeKulutus(double);
+
+private:
+    
+    Asunto as1;
+    Asunto as2;
+    Asunto as3;
+    Asunto as4;
+};
+
+class Katutaso : public Asunto, public Kerros {
+
+public:
+   
+   Katutaso();
+
+   ~Katutaso();
+   
+   void maaritaAsunnot() override; 
+
+   double laskeKulutus(double) override;
+
+private:
+    
+    Asunto as1;
+    Asunto as2;
+};
+
+class Kerrostalo : public Kerros, public Katutaso {
+
+public:
+
+   Kerrostalo() : eka(new Katutaso), toka(), kolmas(){
+   
+        cout<<"Kerrostalo constructor"<<endl;
+   };
+
+   ~Kerrostalo();
+
+   double laskeKulutus(double);
+   
+private:
+
+    Kerros* eka;
+    Kerros toka;
+    Kerros kolmas;
+
+};
+
+#endif
---
+++ teht4/kerrostalo_test.cpp
@@ -0,0 +1,8 @@
+#include "kerrostalo.h"
+
+int main () {
+
+    Kerrostalo kerrostalo;
+    kerrostalo.laskeKulutus(3);
+    return 0;
+}
